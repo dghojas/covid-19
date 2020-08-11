1029f8acb2d729aca4a7f255f61f900211ad28c7
Daniel Godoy <godoyhoja@gmail.com>
2020-08-11 17:57:52 -0400

    Update project covid-19

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,3 +5,4 @@ const covid_countries = 'countries';
 
 export const covidAllGet = () => `${base_url}${covid_all}`;
 export const covidCountriesGet = () => `${base_url}${covid_countries}`;
+export const covidCountriesQuery = q_countries => `${base_url}${covid_countries}${q_countries}`;
--- src/contexts/CountryContext.jsx
+++ src/contexts/CountryContext.jsx
@@ -2,12 +2,12 @@ import React, { createContext, useState, useEffect } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import { covidAllGet, covidCountriesGet } from './../constants';
+import { covidAllGet, covidCountriesGet, covidCountriesQuery } from './../constants';
 
 export const CountryContext = createContext();
 
 const CountryContextProvider = () => {
-
+    const [country, setInputCountry] = useState("worldwide");
     const [countryInfo, setCountryInfo] = useState({});
     const [countries, setCountries] = useState([]);
 
@@ -16,9 +16,8 @@ const CountryContextProvider = () => {
 
     const getCountriesAll = () => {
         fetch(covidAllGet())
-            .then(res => res.json())
-            .then(data => {
-                console.log('All => ' + data);
+            .then((res) => res.json())
+            .then((data) => {
                 setCountryInfo(data);
             })
             .catch(error => console.log(error));
@@ -38,13 +37,43 @@ const CountryContextProvider = () => {
             .catch(error => console.log(error));
     };
 
+    const onCountryChange = async (e) => {
+        const countryCode = e.target.value;
 
+        /*const url =
+            countryCode === "worldwide"
+                ? "https://disease.sh/v3/covid-19/all"
+                : `https://disease.sh/v3/covid-19/countries/${countryCode}`;*/
+
+        const url =
+            countryCode === "worldwide"
+                ? covidAllGet()
+                : covidCountriesQuery(countryCode);
+        await fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setInputCountry(countryCode);
+                setCountryInfo(data);
+            });
+    };
 
     return (
         <FormControl>
-            <Select>
-                <MenuItem>Worldwide</MenuItem>
+            <Select
+                variant="outlined"
+                value={country}
+                onChange={onCountryChange}
+            >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {
+                    countries.map((country) => (
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))
+                }
             </Select>
+
+            <div>todayRecovered: {countryInfo.todayRecovered}</div>
+            <div>recovered: {countryInfo.recovered}</div>
         </FormControl>
     );
 };
