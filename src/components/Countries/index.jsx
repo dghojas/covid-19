945da2b1d9bc0b8c69869d78a63165177434a97e
Daniel Godoy <godoyhoja@gmail.com>
2020-08-12 19:51:16 -0400

    Update project covid-19

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -1,65 +1,88 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Container from '@material-ui/core/Container';
+import { prettyPrintStat } from './../../utils';
+import numeral from 'numeral';
+import { CovidContext } from '../../contexts/CovidContext';
+
 import Cases from '../Cases';
 import Recovered from '../Recovered';
 import Deaths from '../Deaths';
-
-import { CovidContext } from '../../contexts/CovidContext';
 
 const Countries = () => {
     const { country, countryInfo, countries, onCountryChange } = useContext(
         CovidContext
     );
 
+    const [casesType, setCasesType] = useState('cases');
+
     const handleChange = (e) => {
         const idCountry = e.target.value;
         onCountryChange(idCountry);
     };
 
-    console.log(countryInfo.country);
-
     return (
         <Fragment>
             <div className="wrapper">
-                <div className="app__header">
-                    <h1>COVID-19: {countryInfo.country}</h1>
-                    <FormControl className="app__dropdown">
-                        <Select
-                            variant="outlined"
-                            value={country}
-                            onChange={handleChange}
-                        >
-                            <MenuItem key="0" value="worldwide">
-                                Worldwide
-                            </MenuItem>
-                            {countries.map((country, key) => (
-                                <MenuItem key={key} value={country.value}>
-                                    {country.name}
+                <Container>
+                    <div className="app__header">
+                        <div className="app__title">
+                            <div className="blob red"></div>
+                            <h1>
+                                COVID-19:{' '}
+                                {countryInfo.country !== undefined
+                                    ? countryInfo.country
+                                    : 'Worldwide'}
+                            </h1>
+                        </div>
+                        <FormControl className="app__dropdown">
+                            <Select
+                                variant="outlined"
+                                value={country}
+                                onChange={handleChange}
+                            >
+                                <MenuItem key="0" value="worldwide">
+                                    Worldwide
                                 </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </div>
-                <div className="app__stats">
-                    <Cases
-                        title="Cases"
-                        cases={countryInfo.todayCases}
-                        total={countryInfo.cases}
-                    />
-                    <Recovered
-                        title="Recovered"
-                        cases={countryInfo.todayRecovered}
-                        total={countryInfo.recovered}
-                    />
-                    <Deaths
-                        title="Deaths"
-                        cases={countryInfo.todayDeaths}
-                        total={countryInfo.deaths}
-                    />
-                </div>
+                                {countries.map((country, key) => (
+                                    <MenuItem key={key} value={country.value}>
+                                        {country.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className="app__stats">
+                        <Cases
+                            onClick={(e) => setCasesType('cases')}
+                            title="Coronavirus Cases"
+                            isRed
+                            active={casesType === 'cases'}
+                            cases={prettyPrintStat(countryInfo.todayCases)}
+                            total={numeral(countryInfo.cases).format('0.0a')}
+                        />
+                        <Recovered
+                            onClick={(e) => setCasesType('recovered')}
+                            title="Recovered"
+                            active={casesType === 'recovered'}
+                            cases={prettyPrintStat(countryInfo.todayRecovered)}
+                            total={numeral(countryInfo.recovered).format(
+                                '0.0a'
+                            )}
+                        />
+                        <Deaths
+                            onClick={(e) => setCasesType('deaths')}
+                            title="Deaths"
+                            isRed
+                            active={casesType === 'deaths'}
+                            cases={prettyPrintStat(countryInfo.todayDeaths)}
+                            total={numeral(countryInfo.deaths).format('0.0a')}
+                        />
+                    </div>
+                </Container>
             </div>
         </Fragment>
     );
--- src/components/Recovered/index.jsx
+++ src/components/Recovered/index.jsx
@@ -3,14 +3,25 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const Recovered = ({ title, cases, total }) => {
+const Recovered = ({ title, cases, total, active, isRed, ...props }) => {
     return (
-        <Card className="infoBox">
+        <Card
+            onClick={props.onClick}
+            className={`infoBox ${active && 'infoBox--selected'} ${
+                isRed && 'infoBox--red'
+            }`}
+        >
             <CardContent>
                 <Typography color="textSecondary" gutterBottom>
                     {title}
                 </Typography>
-                <h2>{cases}</h2>
+                <h2
+                    className={`infoBox__cases ${
+                        !isRed && 'infoBox__cases--green'
+                    }`}
+                >
+                    {cases}
+                </h2>
                 <Typography className="infoBox__total" color="textSecondary">
                     {total} Total
                 </Typography>
